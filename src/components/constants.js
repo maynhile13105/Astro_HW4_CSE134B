6d4964dc12a0d943b56221ed35c783f7e729691b
maynhile13105 <146885739+maynhile13105@users.noreply.github.com>
2025-03-02 23:08:02 -0800

    try using astro

---
+++ src/components/constants.js
@@ -0,0 +1,44 @@
+import Note from '../components/Note.astro';
+
+const dummyNotesList = [
+    {
+        id: 1,
+        title: "note 1 title",
+        content: "note 1 content",
+        label: "Other",
+    },
+    {
+        id: 2,
+        title: "note 2 title",
+        content: "note 2 content",
+        label: "Personal",
+    },
+    {
+        id: 3,
+        title: "note 3 title",
+        content: "note 3 content",
+        label: "Work",
+
+    },
+    {
+        id: 4,
+        title: "note 4 title",
+        content: "note 4 content",
+        label: "Study",
+    },
+    {
+        id: 5,
+        title: "note 5 title",
+        content: "note 5 content",
+        label: "Study",
+
+    },
+    {
+        id: 6,
+        title: "note 6 title",
+        content: "note 6 content",
+        label: "Personal",
+    },
+]
+
+export default dummyNotesList;
